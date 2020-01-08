7040080bcee48ddb4b371bd112092198f4dce669
Rahul kumar <rahul.kumar8@mindtree.com>
2020-01-08 23:06:18 +0530

    Student controller modified

--- src/main/java/com/mycontrol/test/StudentAdmissionController.java
+++ src/main/java/com/mycontrol/test/StudentAdmissionController.java
@@ -23,7 +23,7 @@ public class StudentAdmissionController {
 	@InitBinder
 	public void initBinder(WebDataBinder binder){
 //		binder.setDisallowedFields(new String[] {"studentMobile"});
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy***MM***dd");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		binder.registerCustomEditor(Date.class, "studentDOB", new CustomDateEditor(dateFormat, false));
 		binder.registerCustomEditor(String.class, "studentName", new StudentNameEditor());
 	}
